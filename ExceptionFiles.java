3d6a529dea88ff7a8b6976ace6283d3eb3d4f563
Emilia Perotto <100068661@csisd.org>
2024-12-02 14:23:09 -0600

    done!!

--- ExceptionFiles.java
+++ ExceptionFiles.java
@@ -22,22 +22,26 @@ public class ExceptionFiles{
     ArrayList<Integer> arr = new ArrayList<Integer>();
     int empty = 0;
     double mean = 0.0;
-    try{
     while(fin.hasNextLine()){
+    try{
       empty = fin.nextInt();
       arr.add(empty);
       mean = mean + empty;
     }
-    }
-    catch(InputMismatchException e){
+    catch(Exception e){
+      if(fin.hasNextLine()){
       System.out.println(fin.nextLine() + " was skipped");
+      }
+      else{
+        System.out.print(" ");
+      }
     }
-
+    }
     FileWriter fout = new FileWriter("output.txt");
     Collections.sort(arr);
     double median = 0.0;
     mean = mean / arr.size();
-    String smean = String.format("Mean : %.3f", mean);
+    String smean = String.format("Mean: %.3f", mean);
     fout.write(smean + "\n");
     if(arr.size()%2==0){
       median = (arr.get((arr.size() - 1)/2) + arr.get(((arr.size()-1)/2)+1)) /2;
@@ -45,10 +49,10 @@ public class ExceptionFiles{
     else{
       median = arr.get((arr.size()-1)/2);
     }
-    String smed = String.format("Median : %.1f", median);
+    String smed = String.format("Median: %.1f", median);
     fout.write(smed + "\n");
-    fout.write("Max " + arr.get(0) + "\n");
-    fout.write("Min " + arr.get(arr.size()-1) + "\n");
-    // fout.close();
+    fout.write("Max: " + arr.get(arr.size()-1) + "\n");
+    fout.write("Min: " + arr.get(0) + "\n");
+    fout.close();
   }
 }
